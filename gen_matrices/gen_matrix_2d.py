f02d70604b3543c5169f8373daceec3f8963cac5
Oliver Evans <oge1@zips.uakron.edu>
2017-04-20 05:36:02 -0400

    Fix small typo in pcnt

--- gen_matrices/gen_matrix_2d.py
+++ gen_matrices/gen_matrix_2d.py
@@ -188,7 +188,7 @@ class KelpScenario(object):
         # Loop through i, j and k in specified order
         for ind1 in range(self._var_lengths[var_order[0]]):
             # Report status
-            pcnt = ind1+1 / self._var_lengths[var_order[0]]
+            pcnt = (ind1+1) / self._var_lengths[var_order[0]]
             print("ind1 = {}/{}: {:.1f}%".format(ind1+1,self._var_lengths[var_order[0]],100*pcnt))
             for ind2 in range(self._var_lengths[var_order[1]]):
                 for ind3 in range(self._var_lengths[var_order[2]]):
