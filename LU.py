2277eef410b5b16184c4c1edeb3fe4caaec3b48f
Christopher Parker <cjp95@zips.uakron.edu>
2017-04-26 09:01:12 -0400

    this is the fastest decomposition, at roughly 4 seconds on my machine. no pivoting

--- LU.py
+++ LU.py
@@ -13,3 +13,22 @@ mat_contents = scio.loadmat(sys.argv[1])
 A = mat_contents['A']
 b = mat_contents['b']
 
+# now, the LU decomposition
+# create L_n (the matrix that eliminates entries below the main diag of A in the nth row)
+A = A.todense()
+a1 = A[:,0]
+
+L = np.empty([len(a1),len(a1)])
+for column in range(len(a1)):
+    for row in np.arange(column,len(a1),1):
+        if row == column:
+            L[row,column] = 1
+        else:
+            L[row,column] = -1*A[row,column]/A[column,column]
+
+U = L@A
+
+X1 = np.linalg.inv(L)@b
+X = np.linalg.inv(U)@X1
+print(X)
+print(time.time() - start_time)
